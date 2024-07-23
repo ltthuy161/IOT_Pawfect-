437f0d3eacd8c1a3039c58fe348a9ca1b0c9c177
Mona Lisa <thuythanhluu161@gmail.com>
2024-07-23 18:37:34 +0700

    hi

--- proposal/build.sh
+++ proposal/build.sh
@@ -1,10 +1,5 @@
 pandoc \
     -o 1.pdf \
-    -V "curdate:$(date '+%Y-%m-%d')" \
-    -M link-citations=true \
     --pdf-engine xelatex \
-    --listings \
-    --highlight-style pygments \
     -Vlang=vi\
-    4.md \
-    
+    1.md 
